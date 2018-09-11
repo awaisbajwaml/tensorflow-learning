1661d8867d1ef4449ae0efd352609f9fadc6c8b4
holger <h.prause@gmx.net>
2018-09-11 15:06:12 +0200

    adding estimator example

--- misc/numpy_slicing.py
+++ misc/numpy_slicing.py
@@ -1,7 +1,6 @@
 import numpy as np
 
 
-
 #Slicing and index bases access:
 #Index access[i]: the dimension is reduced by one.
 #Slicing acess[low:high]: preserves the dimension
@@ -28,7 +27,13 @@ my_array = np.array([[[0,1,2,3,4], [5,6,7,8,9], [10,11,12,13,14]]])
 print(my_array[0][0][0])
 #select elements "0", "1", "2" as 1 dimensionaly array
 print(my_array[0, 0, 0:3])
+
 #select elements "0", "1", "2" as 2 dimensionaly array
 print(my_array[0, 0:1, 0:3])
 #select elements "0", "1", "2" as 3 dimensionaly array
 print(my_array[0:1, 0:1, 0:3])
+#select the fist 3 element - using slicing syntax
+print(my_array[:,:, 0:3])
+#select the fist 3 element - using ellipsis syntax
+print(my_array[..., 0:3])
+
